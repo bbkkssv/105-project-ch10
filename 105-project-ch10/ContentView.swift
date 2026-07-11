edadf0fb3bb0b0d977c94a2a1558fca68a063a2f
RobertVinson <bbkkssv@gmail.com>
2026-07-11 07:59:01 -0500

    Initial Commit

---
+++ 105-project-ch10/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  105-project-ch10
+//
+//  Created by Robert Vinson on 7/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 105-project-ch10/_05_project_ch10App.swift
@@ -0,0 +1,17 @@
+//
+//  _05_project_ch10App.swift
+//  105-project-ch10
+//
+//  Created by Robert Vinson on 7/11/26.
+//
+
+import SwiftUI
+
+@main
+struct _05_project_ch10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
